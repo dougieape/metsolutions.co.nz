8f3aeca6fb72c9612e972f1ab6fc6333c7ec7656
dougieape <46041990+dougieape@users.noreply.github.com>
2025-02-06 11:24:17 +1300

    downloaded latest from server

--- nzta/KaikouraRecentAndForecastRain.php
+++ nzta/KaikouraRecentAndForecastRain.php
@@ -1,5 +1,5 @@
 <?Php
-$file_url = 'https://www.dropbox.com/s/nmc14t1xfhmzrzo/KaikouraRecentAndForecastRain.csv?dl=1';
+$file_url = 'https://www.dropbox.com/s/gy87l0y9jk06tk6/KaikouraRecentAndForecastRain.csv?dl=1';
 echo "<html><body><table border=1>\n\n";
 $f = fopen($file_url, "r");
 while (($line = fgetcsv($f)) !== false) {
